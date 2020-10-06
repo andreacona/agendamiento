d0b9fc2de9b39892cab989949ff275a2562865dc
andrea <andreaignacia.gs@gmail.com>
2020-10-06 10:59:29 -0300

    add reservar y cancelar horas  en vita menu

--- app-agendamiento/src/app/app.module.ts
+++ app-agendamiento/src/app/app.module.ts
@@ -1,44 +1,50 @@
-import {BrowserModule} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import {
   CUSTOM_ELEMENTS_SCHEMA,
   NgModule,
   NO_ERRORS_SCHEMA,
 } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 //COMPONENTS
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
 
 //ANGULAR MATERIAL
 
-import {MatIconModule} from '@angular/material/icon';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ReusableModule} from './reusable/reusable.module';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatNativeDateModule} from '@angular/material/core';
-import {adapterFactory} from 'angular-calendar/date-adapters/moment';
-import {CalendarDateFormatter, CalendarModule, CalendarNativeDateFormatter, DateAdapter, DateFormatterParams} from 'angular-calendar';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReusableModule } from './reusable/reusable.module';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatNativeDateModule } from '@angular/material/core';
+import { adapterFactory } from 'angular-calendar/date-adapters/moment';
+import {
+  CalendarDateFormatter,
+  CalendarModule,
+  CalendarNativeDateFormatter,
+  DateAdapter,
+  DateFormatterParams,
+} from 'angular-calendar';
 import * as moment from 'moment';
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {NgbModalModule} from '@ng-bootstrap/ng-bootstrap';
-import {FlatpickrModule} from 'angularx-flatpickr';
-import {VistaDiariaComponent} from './calendar/vista-diaria/vista-diaria.component';
-import {ModalPopUpComponent} from './reusable/modal-pop-up/modal-pop-up.component';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { VistaDiariaComponent } from './calendar/vista-diaria/vista-diaria.component';
+import { ModalPopUpComponent } from './reusable/modal-pop-up/modal-pop-up.component';
+import { MatMenuModule } from '@angular/material/menu';
 
 /** FORMATEAR HORAS
  * necesita cambio en clases css: .cal-day-view .cal-hour-segment.cal-after-hour-start .cal-time (style.scss)
  */
 class CustomDateFormatter extends CalendarNativeDateFormatter {
-  public dayViewHour({date, locale}: DateFormatterParams): string {
+  public dayViewHour({ date, locale }: DateFormatterParams): string {
     return new Intl.DateTimeFormat('es', {
       hour: 'numeric',
-      minute: 'numeric'
+      minute: 'numeric',
     }).format(date);
   }
 }
@@ -53,16 +59,18 @@ class CustomDateFormatter extends CalendarNativeDateFormatter {
     MatDatepickerModule,
     MatNativeDateModule,
     CalendarModule,
-    CalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: momentAdapterFactory,
-    }, // FORMATEAR HORA
+    CalendarModule.forRoot(
       {
-      dateFormatter: {
-        provide: CalendarDateFormatter,
-        useClass: CustomDateFormatter
+        provide: DateAdapter,
+        useFactory: momentAdapterFactory,
+      }, // FORMATEAR HORA
+      {
+        dateFormatter: {
+          provide: CalendarDateFormatter,
+          useClass: CustomDateFormatter,
+        },
       }
-    }),
+    ),
     BrowserAnimationsModule,
     CommonModule,
     FormsModule,
@@ -72,6 +80,7 @@ class CustomDateFormatter extends CalendarNativeDateFormatter {
     MatButtonModule,
     MatDialogModule,
     MatIconModule,
+    MatMenuModule,
   ],
 
   entryComponents: [ModalPopUpComponent],
@@ -79,8 +88,7 @@ class CustomDateFormatter extends CalendarNativeDateFormatter {
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
-export class AppModule {
-}
+export class AppModule {}
 
 export function momentAdapterFactory(): DateAdapter {
   return adapterFactory(moment);
